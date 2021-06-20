260ef8060e55dfbeb0066ea2fc9c073ad0748f4f
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2021-06-20 10:23:29 -0300

    Question four finished

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,21 @@
 #include "function.h"
+#include <vector>
 
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
-    // TODO: adicione o seu código aqui.
+    std::vector<unsigned int> numbers; // Variable that will contain the fibonacci sequence values
 
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+    int firstValue = 1; // First element of the sequence
+    int secondValue = 1; // Second element of the sequence
+    int sum; // Sum of the first and second elements
+
+    while (firstValue < n) // As long as the first value is less than the flag, the loop continues
+    {
+        numbers.push_back(firstValue); // Add the first element to the end vector
+        sum = firstValue + secondValue; // Add the two elements
+        firstValue = secondValue; // Updates the first element, receiving the value of its successor in the sequence
+        secondValue = sum; // Updates the second element, getting the sum of its current value plus the value of the first element
+    }
+
+    return numbers;
 }
