7e550d074715967f9d18cab39f4ca470c23a8cd0
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2021-06-21 20:34:27 -0300

    Question eight finished

--- autor.md
+++ autor.md
@@ -15,7 +15,7 @@ Preencha os dados abaixo para identificar a autoria do trabalho.
 - [ X ] Minmax
 - [ X ] Inverter
 - [ X ] Filtragem
-- [ ] Ponto em Retângulo 1
+- [ X ] Ponto em Retângulo 1
 - [ ] Ponto em Retângulo 2
 - [ ] Interseção de Retângulos
 
--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -6,7 +6,20 @@
 
 location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
 {
-    // TODO: Coloque aqui seu código.
+    // Conditionals created by analyzing a drawing of a rectangle.
+    if(P.x > IE.x && P.x < SD.x && P.y > IE.y && P.y < SD.y)
+    {
+        // Case where the point does not touch the edge and is inside the rectangle
+        return location_t::INSIDE;
+    } else if(P.x >= IE.x && P.x <= SD.x && P.y == IE.y  || P.x == SD.x && P.y >= IE.y && P.y <= SD.y || 
+    P.x >= IE.x && P.x <= SD.x && P.y == SD.y || P.x == IE.x && P.y >= IE.y && P.y <= SD.y)
+    {   // Case where the stitch is touching the edge
+        // Bottom - Right - Top - Left
+        return location_t::BORDER;
+    } else {
+        // Case where the point is outside the rectangle
+        return location_t::OUTSIDE;
+    }
 
     // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
     return location_t::OUTSIDE;
